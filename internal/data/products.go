dd476bc0a07c5937ab6a7d026fe81615a63cafad
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-12-04 04:41:58 +0600

    Implementing RESTful API
    - Refactored Project Structure
    - Added Product Schema
    - Implemented Product handler

---
+++ internal/data/products.go
@@ -0,0 +1,38 @@
+// Package data contains the products data
+package data
+
+import "time"
+
+// Product defines the structure of a product API
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+// Products Why pointer? - making products mutable
+var Products = []*Product{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
---
+++ pkg/Handlers/ProductHandler.go
@@ -0,0 +1,33 @@
+// Package handlers implements the handlers for Product API
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/ashtishad/go-microservice/internal/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func GetProducts() []*data.Product {
+	return data.Products
+}
+
+func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lp := GetProducts()
+	d, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(d)
+}
