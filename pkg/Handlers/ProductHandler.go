52c69443ef9898b97ee4189bf68171156b8fb4aa
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-12-08 09:55:50 +0600

    Got ID from URL path.
    * used regex
    * error handeled
    * logged id

--- pkg/Handlers/ProductHandler.go
+++ pkg/Handlers/ProductHandler.go
@@ -4,6 +4,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/ashtishad/go-microservice/internal/data"
 )
@@ -21,10 +23,28 @@ func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case http.MethodGet:
-		p.l.Println("Handle GET request")
 		p.getProducts(w, r)
 	case http.MethodPost:
 		p.addProduct(w, r)
+	case http.MethodPut:
+		// expects a product id in the URL path
+		// localhost:8080/products/1
+		rgx := regexp.MustCompile(`/([0-9]+)`)
+		grp := rgx.FindAllStringSubmatch(r.URL.Path, -1)
+		// p.l.Printf("Captured Group : %#v", grp)
+		// grp = [][]string{[]string{"/3", "3"}}
+		if len(grp) != 1 || len(grp[0]) != 2 {
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idStr := grp[0][1] // "3"
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id, can't convert to number", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("got id : ", id)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 	}
@@ -68,3 +88,25 @@ func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	p.l.Printf("Prod : %#v", prod)
 }
+
+// // updateProduct updates an existing product in the datastore
+// func (p *Products) updateProduct(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	p.l.Println("Handling PUT request...")
+
+// 	//  create a new instance of product struct
+// 	prod := &data.Product{}
+
+// 	// deserialize the product struct from the request body
+// 	if err := prod.FromJSON(r.Body); err != nil {
+// 		http.Error(w, "Unable to decode json", http.StatusBadRequest)
+// 		p.l.Printf("Error while decoding json: %v", err)
+// 		return
+// 	}
+
+// 	// update the product in the datastore
+// 	prod.UpdateProductByID()
+
+// 	w.WriteHeader(http.StatusOK)
+// 	p.l.Printf("Prod : %#v", prod)
+// }
